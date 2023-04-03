601e6be6d2a1e6520a027cf81624d25cf127b1e9
khayatcode <a.khayatcode0103@gmail.com>
2023-04-03 16:17:03 -0600

    initial commit

---
+++ Greet.java
@@ -0,0 +1,7 @@
+public class Greet {
+    public static void main(String[] args) {
+        System.out.println("My name is Adam");
+        System.out.println("I am 20 years old");
+        System.out.println("My hometown is Edmonton, Canada");
+    }
+}
